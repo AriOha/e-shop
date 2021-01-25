f97f49bc26d69dfcaba137b2d838c90fe3bdc37f
Ariel <ariel@ohayon.co.il>
2021-01-25 21:11:51 +0200

    Product basic structure

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,25 @@
 public class Product {
+
+    static int startID = 1000;
+    private String productName;
+    protected int catalogId;
+    private double price;
+    private int discount;
+
+    Product(){
+        catalogId=startID++;
+    }
+
+    void changeUnitPrice(double newPrice){
+    }
+    void changeDiscount(double newPrice){
+    }
+
+    @Override
+    public String toString() {
+        return "Product name= " + productName +
+                ", Catalog ID= " + catalogId +
+                ", Price= " + price +
+                ", Discount= " + discount;
+    }
 }
